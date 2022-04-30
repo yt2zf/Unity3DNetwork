f1ad0b905b02bd9987b97f59a39c0e320ff9b2cf
wanyanting <studyYanting@163.com>
2022-04-30 15:25:24 +0800

    chapter2 客户端异步connect和receive

---
+++ Chapter2/Echo.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net.Sockets;
+using UnityEngine.UI;
+using System;
+
+public class Echo : MonoBehaviour {
+
+	//定义套接字
+	Socket socket;
+	//UGUI
+	public InputField InputFeld;
+	public Text text;
+	byte[] readBuff = new byte[1024];
+	string recvStr = "";
+	
+	void Update()
+	{
+		text.text = recvStr;
+	}
+
+	//点击连接按钮
+	public void Connetion()
+	{
+		//Socket
+		socket = new Socket(AddressFamily.InterNetwork,
+			SocketType.Stream, ProtocolType.Tcp);
+		//Connect
+		socket.BeginConnect("127.0.0.1", 8888, ConnectCallBack, socket);
+	
+	}
+
+	void ConnectCallBack(IAsyncResult ar)
+	{
+		try
+		{
+			Socket socketAsync = (Socket)ar.AsyncState;
+			socketAsync.EndConnect(ar);
+			socketAsync.BeginReceive(readBuff, 0, 1024, 0, ReceiveCallBack, socketAsync);
+			
+
+		}catch(Exception e)
+		{
+			Debug.Log("Socket Connect Fail: " + e.ToString());
+		}
+		
+	}
+
+	void ReceiveCallBack(IAsyncResult ar)
+	{
+		try
+		{
+			Socket socketAsync = (Socket)ar.AsyncState;
+			int count = socketAsync.EndReceive(ar);
+			recvStr = System.Text.Encoding.UTF8.GetString(readBuff, 0, count);
+			socketAsync.BeginReceive(readBuff, 0, 1024, 0, ReceiveCallBack, socketAsync);
+		} 
+		catch(Exception e)
+		{
+			Debug.Log("Receive Fail: " + e.ToString());
+		}
+
+	}
+
+	//点击发送按钮
+	public void Send()
+	{
+		//Send
+		string sendStr = InputFeld.text;
+		byte[] sendBytes = System.Text.Encoding.Default.GetBytes(sendStr);
+		socket.Send(sendBytes);
+		//Recv
+		//byte[] readBuff = new byte[1024]; 
+		//int count = socket.Receive(readBuff);
+		//string recvStr = System.Text.Encoding.UTF8.GetString(readBuff, 0, count);
+		//text.text = recvStr;
+		//Close
+		// socket.Close();
+	}
+}
---
+++ Chapter2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace EchoServer
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+			//Socket
+			Socket listenfd = new Socket(AddressFamily.InterNetwork,
+				SocketType.Stream, ProtocolType.Tcp);
+			//Bind
+			IPAddress ipAdr = IPAddress.Parse("127.0.0.1");
+			IPEndPoint ipEp = new IPEndPoint(ipAdr, 8888);
+			listenfd.Bind(ipEp);
+			//Listen
+			listenfd.Listen(0);
+			Console.WriteLine("[服务器]启动成功");
+			
+			while (true) {
+				//Accept
+				Socket connfd = listenfd.Accept();
+				Console.WriteLine("[服务器]Accept");
+				//Receive
+				byte[] readBuff = new byte[1024];
+				int count = connfd.Receive (readBuff);
+				string readStr = System.Text.Encoding.UTF8.GetString (readBuff, 0, count);
+				Console.WriteLine ("[服务器接收]" + readStr);
+				//Send
+				string sendStr = System.DateTime.Now.ToString();
+				byte[] sendBytes = System.Text.Encoding.Default.GetBytes (sendStr);
+				connfd.Send (sendBytes);
+			}
+		}
+	}
+}
