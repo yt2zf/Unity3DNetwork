b8707741b35a40219eeaebe2718cc91839ad5557
wanyanting <studyYanting@163.com>
2022-05-01 22:32:33 +0800

    chapter2 服务端管理连过来的客户端socket，异步accpet，异步receive

--- Chapter2/Program.cs
+++ Chapter2/Program.cs
@@ -1,14 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 
 namespace EchoServer
-{
+{	
+	class ClientState
+	{
+		public Socket socket;
+		public byte[] readBuff = new byte[1024];
+	}
 	class MainClass
 	{
+		// clients
+		static Dictionary<Socket, ClientState> clientStates = new Dictionary<Socket, ClientState>();
 		public static void Main (string[] args)
 		{
 			Console.WriteLine ("Hello World!");
+		
 			//Socket
 			Socket listenfd = new Socket(AddressFamily.InterNetwork,
 				SocketType.Stream, ProtocolType.Tcp);
@@ -19,20 +28,54 @@ namespace EchoServer
 			//Listen
 			listenfd.Listen(0);
 			Console.WriteLine("[服务器]启动成功");
-			
-			while (true) {
-				//Accept
-				Socket connfd = listenfd.Accept();
-				Console.WriteLine("[服务器]Accept");
-				//Receive
-				byte[] readBuff = new byte[1024];
-				int count = connfd.Receive (readBuff);
-				string readStr = System.Text.Encoding.UTF8.GetString (readBuff, 0, count);
-				Console.WriteLine ("[服务器接收]" + readStr);
-				//Send
-				string sendStr = System.DateTime.Now.ToString();
-				byte[] sendBytes = System.Text.Encoding.Default.GetBytes (sendStr);
-				connfd.Send (sendBytes);
+
+			// 异步Accpet
+			listenfd.BeginAccept(AcceptCallBack, listenfd);
+			// 等待
+			Console.ReadLine();
+		}
+
+		public static void AcceptCallBack(IAsyncResult ar)
+		{
+			try
+			{
+				Socket listenfd = (Socket)ar.AsyncState;
+				Socket connfd = (Socket)listenfd.EndAccept(ar);
+				ClientState cs = new ClientState { socket = connfd };
+				clientStates[connfd] = cs;
+				connfd.BeginReceive(cs.readBuff, 0, 1024, 0, ReceiveCallBack, cs);
+				listenfd.BeginAccept(AcceptCallBack, listenfd);
+			} catch(SocketException e)
+			{
+				Console.WriteLine("Socket Accept Fail: " + e.ToString());
+			}
+		}
+
+		public static void ReceiveCallBack(IAsyncResult ar)
+		{
+			try
+			{
+				ClientState cs = (ClientState)ar.AsyncState;
+				Socket clientfd = cs.socket;
+				int count = clientfd.EndReceive(ar);
+				if (count == 0)
+				{
+					clientfd.Close();
+					clientStates.Remove(clientfd);
+					Console.WriteLine("Socket Close");
+					return;
+				}
+				string recvStr = System.Text.Encoding.UTF8.GetString(cs.readBuff, 0, count);
+				Console.WriteLine("[服务器接收]" + recvStr);
+				//Send, 不异步
+				string sendStr = "Echo: " + recvStr;
+				byte[] sendBytes = System.Text.Encoding.Default.GetBytes(sendStr);
+				clientfd.Send(sendBytes);
+				clientfd.BeginReceive(cs.readBuff, 0, 1024, 0, ReceiveCallBack, cs);
+			}
+			catch (SocketException e)
+			{
+				Console.WriteLine("Socket Receive Fail: " + e.ToString());
 			}
 		}
 	}
